e05ab10a11a943f45b1c91fbd665d44ee6418374
shubham <shubhamkamboj2550@gmail.com>
2023-10-19 09:59:34 +0530

    clousure

--- script2.js
+++ script2.js
@@ -30,3 +30,23 @@ const printName = function(){
     
 }
 printName();
+
+
+
+// function ek baar call
+// use of clousure
+function func(){
+    let counter = 0;
+    return function(){
+        if(counter < 1){
+            console.log("Hi You Called me");
+            counter++;
+        }else{
+            console.log("Mai already ek bar call ho chuka hoon!");
+        }
+    }
+}
+
+const myFunc = func();
+myFunc();
+myFunc();
