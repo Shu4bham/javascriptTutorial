c65172b31db279747c78d36309941028df5c9a95
shubham <shubhamkamboj2550@gmail.com>
2023-10-19 09:40:13 +0530

    Function Execution Context and lexical Environment

--- script2.js
+++ script2.js
@@ -1,1 +1,32 @@
-// clousres in js
+// function execution context 
+
+let foo = "foo";
+console.log(foo);
+function getFullName(firstName, lastName){
+    console.log(arguments);
+    let myVar = "var inside func";
+    console.log(myVar);
+    const fullName = firstName + " " + lastName;
+    return fullName;
+}
+
+const personName = getFullName("harshit", "sharma");
+console.log(personName);
+
+
+
+
+// lexical environment, scope chain
+
+const lastName = "Vashistha";
+
+const printName = function(){
+    const firstName = "harshit";
+    function myFunction(){
+        console.log(firstName);
+        console.log(lastName);
+    }
+    myFunction()
+    
+}
+printName();
